a4c243d82136647b507031af0bff88bdd8fc2ada
nirajpoudel18 <nirajpoudel@gmail.com>
2021-02-26 20:29:14 +0545

    updated faces

--- Recognize_faces.py
+++ Recognize_faces.py
@@ -52,10 +52,12 @@ while True:
                      
         name="None matching"
         
-        if(pred[0][1]>0.5):
-            name='Niraj'
-        elif(pred[0][0]>0.5):
+        if(pred[0][0]>0.5):
             name='Newsun'
+        elif(pred[0][1]>0.5):
+            name='Niraj'
+        elif(pred[0][2]>0.5):
+            name='Ashim'
         cv2.putText(frame,name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
     else:
         cv2.putText(frame,"No face found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
